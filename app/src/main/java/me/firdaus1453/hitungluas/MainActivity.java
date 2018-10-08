7cf410b79e4c10087a0da2436afdec8d7b52260b
firdaus1453 <mfirdaus1453@gmail.com>
2018-10-08 22:05:36 +0700

    Initial commit

---
+++ app/src/main/java/me/firdaus1453/hitungluas/MainActivity.java
@@ -0,0 +1,59 @@
+package me.firdaus1453.hitungluas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    /// Membuat variable global
+    TextView txtJdl;
+    Button btnStart;
+    EditText edtText;
+    EditText edtPanjang;
+    EditText edtLebar;
+    TextView txtHasil;
+
+    // Membuat variable global untuk menghitung luas
+    Integer panjang,lebar,hasil;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Inisiasi widget ke dalam variable java agar dapat digunakan
+        txtJdl = findViewById(R.id.txtJudul);
+        edtText = findViewById(R.id.edtText);
+        btnStart = findViewById(R.id.btnStart);
+
+        edtPanjang = findViewById(R.id.edtPanjang);
+        edtLebar = findViewById(R.id.edtLebar);
+        txtHasil = findViewById(R.id.txtHasil);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hitung();
+            }
+        });
+    }
+
+    public void hitung(){
+        // Memasukkan input nama dari user ke dalam variable
+        String isiEditTxt = edtText.getText().toString();
+
+        // Memasukkan input panjang dan lebar dari user ke dalam variable
+        panjang = Integer.valueOf(edtPanjang.getText().toString());
+        lebar = Integer.valueOf(edtLebar.getText().toString());
+
+        // Menghitung luas persegi panjang = P * L
+        hasil = panjang * lebar;
+
+        // Cetak hasil
+        txtHasil.setText("Hello " + isiEditTxt + ", Hasil luasnya adalah : " + hasil);
+    }
+}
